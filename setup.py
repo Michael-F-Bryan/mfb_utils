4393c0be2fb9d6443d53c0df05dd5a9b46b67c82
Michael Bryan <michaelfbryan@gmail.com>
2016-04-18 23:13:05 +0800

    Added a `git tag` command and increment_version() function to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,34 @@
 from setuptools import find_packages, setup
 from Cython.Build import cythonize
+import os
+import sys
+from version import __version__
+
+
+def increment_version():
+    version_info = [int(a) for a in __version__.split('.')]
+    version_info[-1] += 1
+    new_version = '.'.join(version_info)
+
+    # Write the new version to the version.py file
+    with open('version.py', 'w') as f:
+        f.write('__version__ = {}'.format(new_version))
+
+    return new_version
+
+
+# Add a `python setup.py tag` command that will increment the current version
+# And then do `git tag ...`
+if sys.argv[-1] == 'tag':
+    version = increment_version()
+    os.system("git tag -a %s -m 'version %s'" % (version, version))
+    os.system("git push --tags")
+    sys.exit()
 
 
 setup(
     name='utils',
-    version='0.1',
+    version=__version__,
     packages=find_packages(exclude=['tests', 'docs', 'scripts']),
     license='MIT',
     long_description=open('README.rst').read(),
