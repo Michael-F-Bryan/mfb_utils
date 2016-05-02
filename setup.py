614a04a41f785572781f888f5033ab2601c4b815
Michael Bryan <michaelfbryan@gmail.com>
2016-05-03 07:45:41 +0800

    Made py.test ignore test-requirements.txt

--- setup.py
+++ setup.py
@@ -11,12 +11,21 @@
 import sys
 from setuptools import setup
 
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    print('Please install "cython" first')
+    sys.exit(1)
+
 
 def setup_package():
     needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
     sphinx = ['sphinx'] if needs_sphinx else []
-    setup(setup_requires=['six', 'pyscaffold>=2.5a0,<2.6a0'] + sphinx,
-          use_pyscaffold=True)
+    setup(
+            setup_requires=['six', 'pyscaffold>=2.5a0,<2.6a0'] + sphinx,
+            ext_modules=cythonize('utils/math.pyx'),
+            use_pyscaffold=True
+    )
 
 
 if __name__ == "__main__":
